c29dae26522561cdfd43c48e510d0955f9acdfe2
Lidia Micusan <35565172+MicusanL@users.noreply.github.com>
2018-11-09 22:58:19 +0200

    gui buttons, textBox, checkBox

--- src/SlidingWindow/Code.java
+++ src/SlidingWindow/Code.java
@@ -5,12 +5,13 @@ import javax.swing.*;
 public class Code {
     JFileChooser fileChooser = new JFileChooser();
 
-    public void CodeFileUsingLz77() {
+    public void CodeFileUsingLz77(int offset, int length) {
+
         if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             String inputFile;
             inputFile = fileChooser.getSelectedFile().toString();
 
-            System.out.println(inputFile);
+            System.out.println(inputFile + "\n" + length + " " + offset);
         }
     }
 }
--- src/SlidingWindow/UserInterface.java
+++ src/SlidingWindow/UserInterface.java
@@ -3,20 +3,31 @@ package SlidingWindow;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 public class UserInterface {
     private JButton btnCode;
     private JPanel panel1;
     private JButton btnDecode;
+    private JTextField textOffset;
+    private JCheckBox checkBox1;
+    private JTextField textLength;
+    private JList list1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("UserInterface");
-        frame.setContentPane( new UserInterface().panel1);
+        frame.setContentPane(new UserInterface().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+
+
     }
+
     public UserInterface() {
+
+
 
         btnCode.addActionListener(new ActionListener() {
 
@@ -24,11 +35,11 @@ public class UserInterface {
             public void actionPerformed(ActionEvent e) {
 
                 Code lz77Code = new Code();
-                try {
-                    lz77Code.CodeFileUsingLz77();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+                int offset = Integer.parseInt(textOffset.getText());
+                int length = Integer.parseInt(textLength.getText());
+                lz77Code.CodeFileUsingLz77(offset, length);
+
+
             }
         });
 
