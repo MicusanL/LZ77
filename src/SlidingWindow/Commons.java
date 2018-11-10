17383a4c5b8045511f60db2d63025d7f8ef2c84a
Lidia Micusan <35565172+MicusanL@users.noreply.github.com>
2018-11-10 18:40:02 +0200

    Search character sequence - 70% done

--- src/SlidingWindow/Commons.java
+++ src/SlidingWindow/Commons.java
@@ -9,6 +9,7 @@ public class Commons {
     private int borderIndex;
     private int[] window;
     BitReader bitReaderInstance;
+    int bytesLeftToRead;
 
     public Commons(int offset, int length, String inputFile) {
 
@@ -16,33 +17,86 @@ public class Commons {
         borderIndex = Constants.POWER_OF_2[offset];
         window = new int[windowLength];
         bitReaderInstance = new BitReader(inputFile);
+        bytesLeftToRead = bitReaderInstance.fileLength;
     }
 
     public void codeFile() {
 
-        int bytesLeftToRead = bitReaderInstance.fileLength;
-        int lastIndexLookAhead = fillLookAheadBuffer(borderIndex, bytesLeftToRead);
-        for (int i = borderIndex; i < lastIndexLookAhead; i++) {
-            System.out.println(i + " " + window[i]);
+        int lastIndexLookAhead = fillLookAheadBuffer(borderIndex);
+
+        boolean fileFinished = false;
+
+        while (!fileFinished) {
+
+            boolean tookenFinded = false;
+            int length = 0;
+            int offset = 0;
+
+            while (!tookenFinded) {
+
+                int tempIndexSB = searchCharacters(offset, length + 1);
+                if (tempIndexSB == -1) {
+                    tookenFinded = true;
+                } else {
+                    length++;
+                    offset = tempIndexSB;
+                }
+
+            }
+
+            //write length,offset,window[board-length]
+            //window<<length
+            //fillLookAheadBuffer(windowLength - length)?
+
+            if (bytesLeftToRead == 0) {
+                fileFinished = true;
+            }
         }
 
     }
 
-    private int fillLookAheadBuffer(int startIndex, int bytesLeftToRead) {
+    /* index from search-buffer OR -1 */
+    private int searchCharacters(int oldOffset, int sizeBufferToSearch) {
 
-        int lookAheadBufferBytes;
+        for (int i = oldOffset; i >= 0; i--) {
+
+            boolean sequenceFound = true;
+            for (int j = i; j < i + sizeBufferToSearch; j++) {
+                if (window[i + j] != window[borderIndex + j]) {
+                    sequenceFound = false;
+                }
+            }
+
+            if (sequenceFound) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void printLookAheanBuffer(int lastindex) {
+        for (int i = borderIndex; i < lastindex; i++) {
+            System.out.println(i + " " + window[i]);
+        }
+    }
+
+    private int fillLookAheadBuffer(int startIndex) {
 
         if (bytesLeftToRead > windowLength - startIndex) {
 
             for (int i = startIndex; i < windowLength; i++) {
                 window[i] = bitReaderInstance.ReadNBits(Constants.WORD_BITS_NUMBER);
+                bytesLeftToRead--;
             }
             return windowLength;
+
         } else {
 
             int lastIndex = startIndex + bytesLeftToRead;
             for (int i = 0; i < lastIndex; i++) {
                 window[i] = bitReaderInstance.ReadNBits(Constants.WORD_BITS_NUMBER);
+                bytesLeftToRead--;
             }
             return lastIndex;
         }
